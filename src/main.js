77d717b68302e59608632a1caaeb49a9c62c122a
techies03 <90133205+techies03@users.noreply.github.com>
2023-03-13 10:49:55 +0800

    1 binaries uploaded

--- src/main.js
+++ src/main.js
@@ -1,19 +1,15 @@
-import './style.css';
+/*=============== CHANGE BACKGROUND HEADER ===============*/
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
+/*=============== SWIPER INFO ===============*/
+var swiper = new Swiper('.info-swiper', {
+  loop: true,
+});
+/*=============== VALUE ACCORDION ===============*/
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+/*=============== SHOW SCROLL UP ===============*/
+
+/*=============== DARK LIGHT THEME ===============*/
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
