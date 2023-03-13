9ba5a5303be3c0cae6c4b0177b953e57817a30e6
techies03 <90133205+techies03@users.noreply.github.com>
2023-03-13 10:58:10 +0800

    1 binaries uploaded

--- src/main.js
+++ src/main.js
@@ -1,15 +1,24 @@
-/*=============== CHANGE BACKGROUND HEADER ===============*/
+const menuIcon = document.getElementById('menu-icon');
+const slideoutMenu = document.getElementById('slideout-menu');
+const searchIcon = document.getElementById('search-icon');
+const searchBox = document.getElementById('searchbox');
 
-/*=============== SWIPER INFO ===============*/
-var swiper = new Swiper('.info-swiper', {
-  loop: true,
+searchIcon.addEventListener('click', function () {
+  if (searchBox.style.top == '72px') {
+    searchBox.style.top = '24px';
+    searchBox.style.pointerEvents = 'none';
+  } else {
+    searchBox.style.top = '72px';
+    searchBox.style.pointerEvents = 'auto';
+  }
 });
-/*=============== VALUE ACCORDION ===============*/
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
-/*=============== SHOW SCROLL UP ===============*/
-
-/*=============== DARK LIGHT THEME ===============*/
-
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+menuIcon.addEventListener('click', function () {
+  if (slideoutMenu.style.opacity == '1') {
+    slideoutMenu.style.opacity = '0';
+    slideoutMenu.style.pointerEvents = 'none';
+  } else {
+    slideoutMenu.style.opacity = '1';
+    slideoutMenu.style.pointerEvents = 'auto';
+  }
+});
